f908cb06a0ad411c6bc662b410b4e873fab5c8e1
Xghng999 <1369492@qq.com>
2026-06-17 21:28:30 +0800

    Update diy-part2.sh

--- NETGEAR-R6220/immortalwrt-21.02/diy-part2.sh
+++ NETGEAR-R6220/immortalwrt-21.02/diy-part2.sh
@@ -8,7 +8,7 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
+# 修改默认IP
 sed -i 's/192.168.1.1/192.168.166.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
