9066b983f1d76ce718cb397159dab0038fbbfda5
Xghng999 <1369492@qq.com>
2026-06-17 22:48:48 +0800

    Update diy-part1.sh

--- Qihoo-360T7/immortalwrt-23.05/diy-part1.sh
+++ Qihoo-360T7/immortalwrt-23.05/diy-part1.sh
@@ -9,10 +9,10 @@
 #=============================================================
 
 ### 添加第三方订阅源
-# sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git small https://github.com/Xghng/kenzok8-small-package' feeds.conf.default
 # sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
-sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
+# sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
