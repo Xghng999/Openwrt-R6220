b54be8acb4be0d0cc7207c797c7f4b287846a931
Xghng999 <1369492@qq.com>
2026-06-17 17:15:12 +0800

    Create diy-part1-ap.sh

---
+++ xiaomi-mini/ImmortalWrt-21.02/diy-part1-ap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+### 添加第三方订阅源
+# sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git-full small https://github.com/Xghng/kenzok8-small-package' feeds.conf.default
+# sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+# sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
+# echo 'src-git control-weburl https://github.com/Xghng/luci-app-control-weburl' feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
+cd lede
+sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
