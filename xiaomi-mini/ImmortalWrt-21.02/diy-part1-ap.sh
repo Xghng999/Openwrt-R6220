450c2f10579d62edc02861c7703b55db8809cd8c
Xghng999 <1369492@qq.com>
2026-06-17 19:18:00 +0800

    Update diy-part1-ap.sh

--- xiaomi-mini/ImmortalWrt-21.02/diy-part1-ap.sh
+++ xiaomi-mini/ImmortalWrt-21.02/diy-part1-ap.sh
@@ -17,6 +17,6 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
-cd lede
-sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+# cd lede
+# sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
+# ./scripts/feeds update -a && ./scripts/feeds install -a
